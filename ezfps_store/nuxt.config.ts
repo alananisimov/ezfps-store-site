79b414fffe91d2828a14b7fc91ceb09cef7d09ba
alananisimov <66789824+alananisimov@users.noreply.github.com>
2023-07-20 21:03:43 +0000

    sdgfsdf

--- ezfps_store/nuxt.config.ts
+++ ezfps_store/nuxt.config.ts
@@ -1,7 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/supabase'],
+  modules: ['@nuxtjs/supabase', "@nuxtjs/i18n"],
+  i18n: {
+    // Adding English-Canada and Arabic-Egypt
+    // as our supported locales. You can add as
+    // many locales as you want here.
+    locales: ["en", "ru"],
+
+    // Used when active locale is not explicitly defined
+    defaultLocale: "ru",
+
+    // Disabling automatic locale detection (see below)
+    detectBrowserLanguage: true,
+    vueI18n: {
+      fallbackLocale: 'ru',
+      messages: {
+        en: {
+          start_trial: 'We create a real product'
+        },
+        ru: {
+          start_trial : 'Мы создаём реальный продукт'
+        }
+      },
+  },
+},
   extends: '@nuxt-themes/docus',
   postcss: {
     plugins: {
@@ -9,4 +32,7 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  buildModules: [
+    '@nuxtjs/google-fonts'
+  ],
 })
