e4b558371104464a124bdedb8d157f86b1451488
alananisimov <66789824+alananisimov@users.noreply.github.com>
2023-07-21 13:43:04 +0000

    ва

--- ezfps_store/components/Hero.vue
+++ ezfps_store/components/Hero.vue
@@ -5,11 +5,11 @@
   <div class="lg:pr-8 lg:pt-4">
     <div class="lg:max-w-lg my-24 sm:my-48 max-[400px]:my-16">
       <a href="#" class="mb-14 inline-flex justify-between items-center py-1 px-1 pr-4 sm:mb-7 text-sm text-gray-700 bg-gray-100 rounded-full" role="alert">
-        <span class="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">New</span> <span class="text-sm font-medium">Flowbite is out! See what's new</span> 
+        <span class="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">New</span> <span class="text-sm font-medium">ezfps beta вышла. Зацени здесь!</span> 
         <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
     </a>
-    <h1 class="mb-4 text-5xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl">We create a real product</h1>
-    <p class=" mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 dark:text-gray-400">Here at ezfps project we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+    <h1 class="mb-4 text-5xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl">Интуитивно понятный оптимизатор Windows и игр.</h1>
+    <p class=" mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 dark:text-gray-400">Наш главный проект ezfps app помогает оптимизировать пк для игр и других приложений.</p>
     <div class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
 
         <NuxtLink to="/introduction"><a class="inline-flex justify-center items-center py-3 px-7 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
