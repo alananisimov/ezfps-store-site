4925294cab6feddcb0542efd607e3b3ba79b5301
alananisimov <66789824+alananisimov@users.noreply.github.com>
2023-07-21 08:03:42 +0000

    dsffsd

--- ezfps_store/nuxt.config.ts
+++ ezfps_store/nuxt.config.ts
@@ -1,22 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/supabase', "@nuxtjs/i18n"],
-  i18n: {
-    locales: ['en', 'ru'],
-    defaultLocale: 'ru',
-    
-      fallbackLocale: 'ru',
-      messages: {
-        en: {
-          start_trial: 'We create a real product'
-        },
-        ru: {
-          start_trial: 'Мы создаём реальный продукт'
-        }
-      
-    }
-  },
+  modules: ['@nuxtjs/supabase'],
   extends: '@nuxt-themes/docus',
   postcss: {
     plugins: {
