20d4a85f217b71989bd28bc9cbf50854a6306f10
alananisimov <66789824+alananisimov@users.noreply.github.com>
2023-07-21 10:12:24 +0000

    sdfdsf

--- ezfps_store/pages/index.vue
+++ ezfps_store/pages/index.vue
@@ -1,6 +1,8 @@
 <template>
    <title>ezfps: Modern tools for PC opotimization</title>
-   <Meta name="ezfps project: Home page" content="Here at ezfps we create a modern technology products."/>
+   <Meta name="ezfps project: Home page" content="Here at ezfps we create a modern technology products." start_url="https://ezfps.store" prefer_related_applications="true"/>
+   <meta name="theme-light">
+   <meta icon="src: 'icon.png', sizes: '1000x1000'">
    <Hero/>
    <Productlist/>
 </template>
