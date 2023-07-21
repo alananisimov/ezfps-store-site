fcf5e7ad2c68137d957db3edfd6b6e93a42660f7
alananisimov <66789824+alananisimov@users.noreply.github.com>
2023-07-21 10:25:07 +0000

    sdfdf

--- ezfps_store/components/Productlist.vue
+++ ezfps_store/components/Productlist.vue
@@ -8,7 +8,7 @@
         </div>
         <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
           <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
-            <div v-for="feature in features" :key="feature.name" class="relative pl-16">
+            <div v-for="feature in features" :key="feature.name" class="relative justify-center mx-auto">
               <dt class="text-base font-semibold leading-7 text-gray-900">
                 <div class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
                   <img :src="feature.icon" :alt="feature.alt">
@@ -40,20 +40,6 @@
         'Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.',
       icon: 'telegram.svg',
       alt: "telegram logo"
-    },
-    {
-      name: 'Simple queues',
-      description:
-        'Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.',
-      icon: ArrowPathIcon,
-      alt: ""
-    },
-    {
-      name: 'Advanced security',
-      description:
-        'Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.',
-      icon: FingerPrintIcon,
-      alt: ""
-    },
+    }
   ]
   </script>
