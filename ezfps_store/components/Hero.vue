f97063e0745a5d74951c0a2d7884e753e2a39f0d
alananisimov <66789824+alananisimov@users.noreply.github.com>
2023-07-22 12:12:50 +0000

    sdff

--- ezfps_store/components/Hero.vue
+++ ezfps_store/components/Hero.vue
@@ -3,7 +3,7 @@
 <div class="mx-auto max-w-7xl px-6 lg:px-8">
 <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
   <div class="lg:pr-8 lg:pt-4">
-    <div class="lg:max-w-lg my-24 sm:my-48 max-[400px]:my-16">
+    <div class="lg:max-w-lg my-16 sm:my-48 max-[400px]:my-12">
       <NuxtLink to="/introduction"><a class="mb-14 inline-flex justify-between items-center py-1 px-1 pr-4 sm:mb-7 text-sm text-gray-700 bg-gray-100 rounded-full" role="alert">
         <span class="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">New</span> <span class="text-sm font-medium">ezfps вышел. Зацени сейчас!</span> 
         <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
