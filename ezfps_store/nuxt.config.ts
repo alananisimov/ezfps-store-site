ca994025309c37e935d880a5c0c2344af6f51d3b
alananisimov <66789824+alananisimov@users.noreply.github.com>
2023-07-23 13:26:55 +0000

    sdff

--- ezfps_store/nuxt.config.ts
+++ ezfps_store/nuxt.config.ts
@@ -3,6 +3,13 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/supabase', '@nuxtjs/color-mode'],
   extends: '@nuxt-themes/docus',
+  head: {
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: 'https://www.ezfps.store/icon.png' }
+    ]
+
+  },
+
   colorMode: {
     classSuffix: ''
   },
