86a2d2b1275e9a328feca521eee55a6662cf5b33
alananisimov <66789824+alananisimov@users.noreply.github.com>
2023-07-26 13:54:48 +0300

    ываа

--- ezfps_store/pages/download.vue
+++ ezfps_store/pages/download.vue
@@ -2,8 +2,8 @@
     <section class="bg-white dark:bg-gray-900">
   <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6 sm:pt-64">
       <div class="max-w-screen-md mb-8 lg:mb-16">
-          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Designed for business teams like yours</h2>
-          <p class="text-gray-500 sm:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Лучшее предложение именно для вас</h2>
+          <p class="text-gray-500 sm:text-xl dark:text-gray-400">Здесь в ezfps мы предлагаем лучший продукт на рынке</p>
       </div>
       <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
           <div class="border-2 rounded-[30px] border-black p-8 hover:bg-gray-200">
