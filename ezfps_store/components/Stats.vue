154eae731ce666acb735d2573387e355025d7295
Alexander <66789824+alananisimov@users.noreply.github.com>
2023-07-27 23:02:52 +0300

    sdff

--- ezfps_store/components/Stats.vue
+++ ezfps_store/components/Stats.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="w-full px-4 pt-16">
+    <h2 class="text-4xl font-bold dark:text-white my-16">FAQ</h2>
       <div class="mx-auto w-full max-w-md rounded-2xl bg-white p-2">
         <Disclosure v-slot="{ open }">
           <DisclosureButton
             class="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
           >
-            <span>What is your refund policy?</span>
+            <span>Что такое ezfps?</span>
             <ChevronUpIcon
               :class="open ? 'rotate-180 transform' : ''"
               class="h-5 w-5 text-purple-500"
             />
           </DisclosureButton>
           <DisclosurePanel class="px-4 pt-4 pb-2 text-sm text-gray-500">
-            If you're unhappy with your purchase for any reason, email us within
-            90 days and we'll refund you in full, no questions asked.
+            Ezfps это программа для оптимизации пк с <a href="https://github.com/alananisimov/ezfps-app-site" class="underline">открытым исходным кодом</a> основанная на .ps1 скриптах. Наша цель создать лучший из возможных оптимизаторов для Windows. 
           </DisclosurePanel>
         </Disclosure>
         <Disclosure as="div" class="mt-2" v-slot="{ open }">
@@ -27,7 +27,7 @@
             />
           </DisclosureButton>
           <DisclosurePanel class="px-4 pt-4 pb-2 text-sm text-gray-500">
-            No.
+            Свяжитесь с нами через в <a href="https://t.me/hollz69" target="_blank" class="underline">telegram</a>. Мы постараемя ответить вам скорее 
           </DisclosurePanel>
         </Disclosure>
       </div>
