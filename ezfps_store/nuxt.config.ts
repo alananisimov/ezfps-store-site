28af76f4546f4fff4714e0606e53a01b3e265688
Alexander <66789824+alananisimov@users.noreply.github.com>
2023-07-27 21:53:51 +0300

    dfgg

--- ezfps_store/nuxt.config.ts
+++ ezfps_store/nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/supabase', '@nuxtjs/color-mode'],
   extends: '@nuxt-themes/docus',
+  ssr: true,
   postcss: {
     plugins: {
       tailwindcss: {},
