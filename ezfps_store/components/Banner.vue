30a47516521c91138cb3026ef9ab034264e3480c
“Alexander” <alananisimov@gmail.com>
2023-07-31 15:28:10 +0300

    new

--- ezfps_store/components/Banner.vue
+++ ezfps_store/components/Banner.vue
@@ -10,7 +10,7 @@
         <p class="text-sm leading-6 text-gray-900">
           <strong class="font-semibold">Бета-тест 2023</strong><svg viewBox="0 0 2 2" class="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true"><circle cx="1" cy="1" r="1" /></svg><span class="hidden sm:inline-flex">Присоединяйся к нам.</span>
         </p>
-        <a href="/signup" class="flex-none text-white rounded-full bg-black px-3.5 py-1 text-sm font-semibold hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900">Зарегистрируйся сейчас! <span aria-hidden="true">&rarr;</span></a>
+        <a href="/signup" class="flex-none text-white rounded-full bg-black px-3.5 py-1 text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900">Зарегистрируйся сейчас! <span aria-hidden="true">&rarr;</span></a>
       </div>
       <div class="flex flex-1 justify-end">
         <button type="button" class="-m-3 p-3 focus-visible:outline-offset-[-4px]" @click="open = false">
