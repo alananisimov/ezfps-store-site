efd7bf6a7c28d9950dac739bcf4a932882fa6654
“Alexander” <alananisimov@gmail.com>
2023-07-31 12:22:23 +0300

    new

--- ezfps_store/components/Pudge.vue
+++ ezfps_store/components/Pudge.vue
@@ -4,14 +4,14 @@ import { OrbitControls, GLTFModel } from '@tresjs/cientos'
 <template>
 <div class="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 hidden lg:block h-[48rem] mt-20">
     <TresCanvas shadows alpha>
-      <TresPerspectiveCamera :position="[5, 5, 5]" minZoom="1" :args="[45, 1, 0.1, 1000]"/>
+      <TresPerspectiveCamera :position="[4, 4, 4]" minZoom="1" :args="[45, 1, 0.1, 1000]"/>
       <OrbitControls auto-rotate :enable-zoom="false" :enable-damping="true" :enable-pan="false"/>
       <Suspense>
         <GLTFModel path="/scene.gltf" ref="modelRef" />
         
       </Suspense>
-      <TresDirectionalLight :position="[5, 5, 5]" :intensity="5" cast-shadow />
-      <TresDirectionalLight :position="[-5, 5, -5]" :intensity="5" cast-shadow />
+      <TresDirectionalLight :position="[4, 4, 4]" :intensity="5" cast-shadow />
+      <TresDirectionalLight :position="[-4, 4, -4]" :intensity="5" cast-shadow />
     </TresCanvas>
 </div>
 </template>
