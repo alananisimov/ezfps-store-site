18ab0107d015e35ee095d453dee46faef6ff56a1
“Alexander” <alananisimov@gmail.com>
2023-07-31 14:02:36 +0300

    new

--- ezfps_store/components/Productlist.vue
+++ ezfps_store/components/Productlist.vue
@@ -2,7 +2,7 @@
   <div class="bg-white py-24 sm:py-32">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
       <div class="mx-auto max-w-2xl lg:text-center">
-        <h2 class="text-base font-semibold leading-7 text-indigo-600">Проекты</h2>
+        <h2 class="text-base font-semibold leading-7 text-orange-600">Проекты</h2>
         <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Наши проекты и системы</p>
         <p class="mt-6 text-lg leading-8 text-gray-600">Наши продукты максимально эффективно выполняют свою цель - оптимизируют ваш пк и удаляют лишние кэш файлы</p>
       </div>
