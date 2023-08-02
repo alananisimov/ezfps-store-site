27fd612e254530f63ceb37f8d6f981ea9f44e89a
alananisimov <alananisimov@gmail.com>
2023-08-02 13:27:21 +0300

    sdsdff

--- ezfps_store/nuxt.config.ts
+++ ezfps_store/nuxt.config.ts
@@ -1,9 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/supabase', '@nuxtjs/color-mode', '@nuxthq/ui', "@tresjs/nuxt", 'nuxt-headlessui'],
-  extends: '@nuxt-themes/docus',
-  
+  modules: ['@nuxtjs/supabase', '@nuxtjs/color-mode', '@nuxthq/ui', "@tresjs/nuxt", 'nuxt-headlessui', '@nuxtjs/robots', 'nuxt-og-image'],
+  extends: ['@nuxt-themes/docus', 'nuxt-seo-kit'],
+  runtimeConfig: {
+    public: {
+      siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'https://example.com',
+      siteName: 'ezfps store',
+      siteDescription: 'ezfps: Оптимизируй ПК без проблем',
+      language: 'ru', // prefer more explicit language codes like `en-AU` over `en`
+    }
+  },
   ssr: true,
   postcss: {
     plugins: {
