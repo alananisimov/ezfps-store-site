3fa736c95c430bc26399534e15a0b106cb705a0c
alananisimov <alananisimov@gmail.com>
2023-10-28 21:31:05 +0300

    added

--- ezfps_store/nuxt.config.ts
+++ ezfps_store/nuxt.config.ts
@@ -15,19 +15,7 @@ export default defineNuxtConfig({
   ssr: true,
   vite: {
     build: {
-      rollupOptions: {
-        output: {
-          manualChunks(id) {
-            if (id.includes("node_modules")) {
-              return id
-                .toString()
-                .split("node_modules/")[1]
-                .split("/")[0]
-                .toString();
-            }
-          },
-        },
-      },
+      chunkSizeWarningLimit: 1600,
     },
   },
   postcss: {
