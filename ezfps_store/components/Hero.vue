e0b65c9efbf36aa8f1dc07f3619f6a11d830191d
alananisimov <alananisimov@gmail.com>
2023-11-01 15:17:41 +0300

    minor design changes

--- ezfps_store/components/Hero.vue
+++ ezfps_store/components/Hero.vue
@@ -13,7 +13,7 @@
     <p class=" mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 dark:text-gray-400">Наш главный проект ezfps app помогает оптимизировать пк для игр и других приложений.</p>
     <div class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
 
-      <a href="https://t.me/ezfps_bot" class="inline-flex justify-center items-center py-3 px-7 text-base font-medium text-center text-white rounded-lg bg-orange-500 hover:bg-orange-600 focus:ring-4 focus:ring-orange-400 dark:focus:ring-primary-900 mx-4 sm:mx-0">
+      <a href="https://t.me/ezfps_bot" class="inline-flex justify-center items-center py-3 px-7 text-base font-medium text-center text-white rounded-lg bg-orange-500 hover:bg-orange-600 focus:ring-4 focus:ring-orange-400 dark:focus:ring-primary-900 mx-2 sm:mx-0">
             Начать бесплатный период
             <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
         </a>
